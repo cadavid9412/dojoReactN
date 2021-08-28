909b6691b5ebaffc4ab417fac9fbeff12c7d3e3c
cadavid <sebastian.cadavid9412@gmail.com>
2021-08-28 10:14:25 -0500

    dojo

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { HelloWorldScreen } from './src/screens/helloWorld';
+import {BasicCounterScreen} from './src/screens/basic-counter'
+export default function App() {
+  return (
+    //<HelloWorldScreen></HelloWorldScreen>
+    <BasicCounterScreen></BasicCounterScreen>
+  
+    );
+}
+
+
